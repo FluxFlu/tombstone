dff2f10922a2dbcfaf4130d5fae8cd670f58f323
FluxFlu <adamrlukies@gmail.com>
2023-02-22 20:19:09 -0500

    Removed unnecessary lines

--- TMB/optimization/optimize.js
+++ TMB/optimization/optimize.js
@@ -2,9 +2,7 @@ const { wasm_optimize } = require("./wasm_optimize");
 
 function optimize(file, language) {
     switch(language) {
-        case "bf":
-            return bf_optimize(file);
-        case "wasm":
+       case "wasm":
             return wasm_optimize(file);
     }
     return file;
